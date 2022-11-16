c911e563754db938c907698d6824140659e2ddcb
Tommy Egbe <tabe_e@yahoo.ca>
2022-11-16 12:02:20 +0100

    removed nodemon from start script

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('common'));
-mongoose.connect('mongodb://localhost:27017/commentsSection', { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/commentsSection', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(express.static('public'));
 
       
